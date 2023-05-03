8c90323c9be100c0c0b3a6d1c91abac7b49851f4
YaDmitryE <120764954+YaDmitryE@users.noreply.github.com>
2023-05-03 16:27:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# py_lesson_3
+ py_lesson_3
---
+++ task_2.py
@@ -0,0 +1,12 @@
+# запрашиваем ввод этапов развития человека
+stage1 = input("Введите первый этап развития человека: ")
+stage2 = input("Введите второй этап развития человека: ")
+stage3 = input("Введите третий этап развития человека: ")
+stage4 = input("Введите четвертый этап развития человека: ")
+stage5 = "Homo sapiens sapiens"
+
+# выводим этапы развити человека
+print(stage1, stage2, stage3, stage4, stage5, sep=" => ")
+
+# key:
+# Australopithecus=>Homo_habilis=>Homo_erectus=>Homo sapiens=>Homo_sapiens_sapiens
